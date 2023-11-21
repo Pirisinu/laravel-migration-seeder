d9a2af2f3fcee1b8a06b686f84f78c470db776be
Pirisinu <palazzialessandro2@gmail.com>
2023-11-21 17:07:43 +0100

    [ADD]@ForEach->Trains

--- resources/views/all_ticket.blade.php
+++ resources/views/all_ticket.blade.php
@@ -1,3 +1,34 @@
 <main>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Azienda</th>
+            <th scope="col">Stazione di partenza</th>
+            <th scope="col">Stazione di arrivo</th>
+            <th scope="col">Orario di partenza</th>
+            <th scope="col">Orario di arrivo</th>
+            <th scope="col">Codice Treno</th>
+            <th scope="col">Numero Carrozze</th>
+            <th scope="col">Nell'orario</th>
+            <th scope="col">Cancellato</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <td>{{$train->agency}}</td>
+                    <td>{{$train->departure_station}}</td>
+                    <td>{{$train->arrival_station}}</td>
+                    <td>{{$train->departure_time}}</td>
+                    <td>{{$train->arrival_time}}</td>
+                    <td>{{$train->train_code}}</td>
+                    <td>{{$train->number_of_carriages}}</td>
+                    <td>{{$train->in_the_timetable}}</td>
+                    <td>{{$train->deleted}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+      </table>
+
     @yield('single_ticket')
 </main>
